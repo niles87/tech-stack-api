4f0353ce19e28f17099e9e001afd5ecb6e153f59
Niles87 <nbing19@gmail.com>
2021-01-20 20:49:27 -0800

    added basic database model

---
+++ tech-stack/db.py
@@ -0,0 +1,44 @@
+from enum import unique
+from flask_sqlalchemy import SQLAlchemy
+import click
+from flask import current_app, g
+from flask.cli import cli, with_appcontext
+
+
+db = SQLAlchemy(current_app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(55), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self) -> str:
+        return '<User %r' % self.name
+
+
+class Stock(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    available_stock = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self) -> str:
+        return '<Stock %r' % self.name
+
+
+def init_db():
+    db.create_all()
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    init_db()
+    click.echo("Initialized the db")
+
+
+def init_app(app):
+    app.cli.add_command(init_db_command)
